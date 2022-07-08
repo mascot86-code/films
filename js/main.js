d6697ced96128d34ba3e1cd62967f313dba9720a
mascot86 <mascot86@mail.ru>
2022-07-08 14:15:52 +0300

    update project and test adaptive in index.html

--- js/main.js
+++ js/main.js
@@ -1,27 +1,26 @@
 /* Задание на урок:
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
+1) Первую часть задания повторить по уроку
 
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
 
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
 
-Проверить, чтобы все работало без ошибок в консоли */
+P.S. Функции вызывать не обязательно*/
 
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+  }
+}
+
+start();
 
 let personalMovieDB = {
   count: numberOfFilms,
@@ -31,12 +30,56 @@ let personalMovieDB = {
   privat: false,
 };
 
-let a = prompt("Один из последних просмотренных фильмов?", "");
-let b = +prompt("На сколько оцените его?", "");
-let c = prompt("Один из последних просмотренных фильмов?", "");
-let d = +prompt("На сколько оцените его?", "");
+console.log(personalMovieDB.count);
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+function writeYourGenres() {
+  let count = 0;
+  while (count < 3) {
+    personalMovieDB.genres.push(
+      prompt(`Ваш любимый жанр под номером ${count + 1}`),
+    );
+    count++;
+  }
+}
 
-console.log(personalMovieDB);
+writeYourGenres();
+
+function rememberMyFilm() {
+  for (let i = 0; i < 2; i++) {
+    let a = prompt("Один из последних просмотренных фильмов?", "");
+    let b = +prompt("На сколько оцените его?", "");
+    if (a !== "" && b !== "" && a !== null && b !== null && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log("Done");
+    } else {
+      console.log("Error");
+      i--;
+    }
+  }
+}
+
+rememberMyFilm();
+
+function filmCount() {
+  if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+  } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    alert("Вы классический зритель");
+  } else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
+  } else {
+    alert("Произошла ошибка");
+  }
+}
+
+filmCount();
+
+function showMyDB(privat) {
+  if (privat === false) {
+    console.log(personalMovieDB);
+  } else {
+    console.log("This object is privat. Access denided!");
+  }
+}
+
+showMyDB(personalMovieDB.privat);
